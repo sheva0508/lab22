0b8adb2c5fddb748d08fafc1bf129089939d109d
{GITHUB_USERNAME} <{GITHUB_USERNAME}>
2023-09-25 12:00:58 +0700

    added hello_word.cpp

---
+++ hello_word.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib> // для system
+using namespace std;
+
+int main() 
+{ 
+    cout << "Hello, world!" << endl;
+    system("pause"); // Только для тех, у кого MS Visual Studio
+    return 0; 
+}
+
